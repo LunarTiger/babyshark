259729595a1f27f9f20f11ee21ae4a4f2af136a5
Ryan Fowler-Hughes <lunartiger@live.com>
2019-02-16 13:24:03 -0500

    :tada:

--- babyshark.js
+++ babyshark.js
@@ -1,3 +1,3 @@
-var sharks = ["Baby","Daddy","Mommy","Grandpa","Grandma"].map(s=>(s+" Shark"+(" doo".repeat(6))+"<br>").repeat(3)+s+" Shark!<br><br>").join("");
-var actions = ["Let's go hunt","Run away","Safe at last","It's the end"].map(a=>(a+(" doo".repeat(6))+"<br>").repeat(3)+a+"!<br><br>").join("");
-document.body.innerHTML = sharks+actions;
+var sharks = ["Baby","Daddy","Mommy","Grandpa","Grandma"].map(s=>(s+" Shark"+(" doo".repeat(6))+"/n").repeat(3)+s+" Shark!\n\n").join("");
+var actions = ["Let's go hunt","Run away","Safe at last","It's the end"].map(a=>(a+(" doo".repeat(6))+"\n").repeat(3)+a+"!\n\n").join("");
+document.body.innerHTML = "<pre>"+sharks+actions+"</pre>";
