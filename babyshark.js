97808c2069297f8f45a33fec414d39c5527c537a
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2024-11-29 03:05:08 +0000

    🎉

--- babyshark.js
+++ babyshark.js
@@ -1,3 +1,1 @@
-var sharks = ["Baby","Daddy","Mommy","Grandpa","Grandma"].map(s=>(s+" Shark,"+(" doo".repeat(6))+"\n").repeat(3)+s+" Shark!\n\n").join("");
-var actions = ["Let's go hunt","Run away","Safe at last","It's the end"].map(a=>(a+","+(" doo".repeat(6))+"\n").repeat(3)+a+"!\n\n").join("");
-document.body.innerHTML = "<pre>"+sharks+actions+"</pre>";
+document.body.innerHTML = "<pre>"+["Baby","Daddy","Mommy","Grandpa","Grandma"].map(s=>(s+" Shark,"+(" doo".repeat(6))+"\n").repeat(3)+s+" Shark!\n\n").join("")+["Let's go hunt","Run away","Safe at last","It's the end"].map(a=>(a+","+(" doo".repeat(6))+"\n").repeat(3)+a+"!\n\n").join("")+"</pre>";
